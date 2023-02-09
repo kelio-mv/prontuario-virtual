2ecb930668a0607485e097c0dc629faf6639bd1b
Kelio <keliojosue2@gmail.com>
2023-02-08 22:20:44 -0300

    Removed animations in DropdownSelect

--- src/utils/DropdownSelect.jsx
+++ src/utils/DropdownSelect.jsx
@@ -1,47 +1,28 @@
-import React from "react";
+import { useState } from "react";
 import "./DropdownSelect.css";
 
-export default class DropdownSelect extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayOptions: false,
-    };
-    this.optionsRef = React.createRef();
-  }
+export default function DropdownSelect(props) {
+  const [showOptions, setShowOptions] = useState(false);
 
-  toggleState = () => {
-    if (this.state.displayOptions) {
-      this.optionsRef.current.classList.add("disabled");
-      setTimeout(() => this.setState({ displayOptions: false }), 250);
-    } else {
-      this.setState({ displayOptions: true }, () => {
-        setTimeout(() => this.optionsRef.current.classList.remove("disabled"), 0);
-      });
-    }
-  };
-
-  render() {
-    return (
-      <div className="dropdown-select" onClick={this.toggleState}>
-        <div className="header">
-          <p className="label">{this.props.label}</p>
-          <svg width="15" height="8">
-            <polygon points="0,0 14,0, 7,7" fill="var(--bg-secondary)" />
-          </svg>
-        </div>
-        <p>{this.props.value}</p>
-
-        {this.state.displayOptions && (
-          <div ref={this.optionsRef} className="options disabled">
-            {this.props.options.map((e, i) => (
-              <p key={i} onClick={() => this.props.onChange(e)}>
-                {e}
-              </p>
-            ))}
-          </div>
-        )}
+  return (
+    <div className="dropdown-select" onClick={() => setShowOptions(!showOptions)}>
+      <div className="header">
+        <p className="label">{props.label}</p>
+        <svg width="15" height="8">
+          <polygon points="0,0 14,0, 7,7" fill="var(--bg-secondary)" />
+        </svg>
       </div>
-    );
-  }
+      <p>{props.value}</p>
+
+      {showOptions && (
+        <div className="options">
+          {props.options.map((e, i) => (
+            <p key={i} onClick={() => props.onChange(e)}>
+              {e}
+            </p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
