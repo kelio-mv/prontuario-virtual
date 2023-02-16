2506f4e4d10209005d2ac9a140503a42f4885b5b
Kelio <keliojosue2@gmail.com>
2023-02-15 22:38:25 -0300

    Starting integration with Google Drive

--- src/App.jsx
+++ src/App.jsx
@@ -1,145 +1,98 @@
 import React from "react";
-import MedicalRecord from "./MedicalRecord";
-import RegistrationEditor from "./RegistrationEditor";
-import AnamnesisEditor from "./AnamnesisEditor";
-import SessionLogEditor from "./SessionLogEditor";
 import storage from "./storage";
-import utils from "./utils";
-import "./App.css";
-import "./ModalBodyElements.css";
+
+const gconfig = {
+  CLIENT_ID: "868395197081-oesl4h2ukpvdfidobi31693t43ep9bvp.apps.googleusercontent.com",
+  API_KEY: "AIzaSyBF_34fpjsU-arA1GZ_umk2aYqVH4R1goQ",
+  DISCOVERY_DOC: "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
+  SCOPES: "https://www.googleapis.com/auth/drive.appdata",
+  tokenClient: null,
+};
 
 export default class App extends React.Component {
   constructor() {
     super();
-    // The "pid" (Patient Identifier) is the index of the selected patient in
-    // the stored array of patients.
     this.state = {
-      patientsNames: storage.getAllPatientsNames(),
-      nameFilter: "",
-      displayedModal: null,
-      pid: null,
+      gapiInited: false,
+      gisInited: false,
     };
   }
 
-  getFilteredNames() {
-    return this.state.patientsNames.filter((name) => {
-      name = utils.removeAccents(name).toLowerCase();
-      const nameFilter = utils.removeAccents(this.state.nameFilter).toLowerCase().trim();
-      return name.includes(nameFilter);
-    });
+  componentDidMount() {
+    this.loadScript("https://apis.google.com/js/api.js", this.gapiLoaded);
+    this.loadScript("https://accounts.google.com/gsi/client", this.gisLoaded);
   }
 
-  getDisplayedModal(args) {
-    switch (this.state.displayedModal) {
-      case "MedicalRecord":
-        return <MedicalRecord {...args} />;
-
-      case "RegistrationEditor":
-        return <RegistrationEditor {...args} />;
-
-      case "AnamnesisEditor":
-        return <AnamnesisEditor {...args} />;
-
-      case "SessionLogEditor":
-        return <SessionLogEditor {...args} />;
-    }
+  loadScript(src, callback) {
+    const script = document.createElement("script");
+    script.onload = callback;
+    script.src = src;
+    document.body.appendChild(script);
+    // script.async = true;
+    // script.defer = true;
   }
 
-  closeModal = () => {
-    this.setState({
-      displayedModal: null,
-      pid: null,
-      patientsNames: storage.getAllPatientsNames(),
-    });
+  gapiLoaded = () => {
+    gapi.load("client", this.initializeGapiClient);
   };
+
+  gisLoaded = () => {
+    gconfig.tokenClient = google.accounts.oauth2.initTokenClient({
+      client_id: gconfig.CLIENT_ID,
+      scope: gconfig.SCOPES,
+      callback: "",
+    });
+    this.setState({ gisInited: true });
+  };
+
+  initializeGapiClient = async () => {
+    await gapi.client.init({
+      apiKey: gconfig.API_KEY,
+      discoveryDocs: [gconfig.DISCOVERY_DOC],
+    });
+    this.setState({ gapiInited: true });
+  };
+
+  handleAuthClick() {
+    gconfig.tokenClient.callback = async (resp) => {
+      if (resp.error !== undefined) {
+        throw resp;
+      }
+      // Redirect to Main page ----
+    };
+    // Prompt the user to select a Google account, if there are multiple accounts saved or
+    // if the user has not granted the necessary permissions yet.
+    // Request the necessary permissions from the user, if they have not granted them yet
+    gconfig.tokenClient.requestAccessToken({ prompt: "" });
+  }
 
   render() {
     return (
       <>
-        {/* Header */}
-        <header id="header">
-          <img src="profile-picture.png" alt="profile picture" />
-        </header>
-
-        {/* Inputs container */}
-        <div id="inputs-container">
-          <div className="input-box grow">
-            <img src="search.png" alt="search patient" />
-            <input
-              type="text"
-              placeholder="Pesquisar..."
-              value={this.state.nameFilter}
-              onInput={(e) => {
-                const raw = e.target.value;
-                const value = raw.trim() + (raw.endsWith(" ") && raw.length > 1 ? " " : "");
-                this.setState({ nameFilter: value });
-              }}
-            />
-          </div>
-
-          <div
-            className="add-patient input-box pointer"
-            onClick={() => this.setState({ displayedModal: "RegistrationEditor" })}
-          >
-            <img src="add-patient.png" alt="add patient" />
-            <p>Novo Paciente</p>
-          </div>
-        </div>
-
-        {/* Main */}
-        <main id="main-area">
-          {this.getFilteredNames().map((name, i) => (
-            <div key={i} className="row" onClick={() => this.setState({ pid: i })}>
-              <img src="patient-picture.png" alt="patient" />
-              <p
-                className="name pointer"
-                onClick={() => this.setState({ displayedModal: "MedicalRecord" })}
-              >
-                {name}
-              </p>
-              <div className="grow"></div>
-              <div
-                className="input-box pointer"
-                onClick={() => this.setState({ displayedModal: "RegistrationEditor" })}
-              >
-                Cadastro
-              </div>
-              <div
-                className="input-box pointer"
-                onClick={() => this.setState({ displayedModal: "AnamnesisEditor" })}
-              >
-                Anamnese
-              </div>
-              <div
-                className="input-box pointer"
-                onClick={() => this.setState({ displayedModal: "SessionLogEditor" })}
-              >
-                Evolução
-              </div>
-            </div>
-          ))}
-        </main>
-
-        {/* Modal */}
-        {this.getDisplayedModal({
-          pid: this.state.pid,
-          onClose: this.closeModal,
-        })}
+        {this.state.gapiInited && this.state.gisInited && (
+          <button onClick={this.handleAuthClick}>Entrar com o Google</button>
+        )}
       </>
     );
   }
 }
 
-// Deletar registros de sessões e pacientes
-// Integrar com o Drive (fazer caching)
-// Adicionar opção de anexar documentos no paciente
-// Adicionar opção imprimir prontuário
-// Tornar pago (R$ 20 / mes)
+// Ao clicar em Entrar com o Google...
+// Verifique se já existe um token
+// Se existir, {func} verifique se ele não expirou.
+// Se tiver expirado, faça o request de um novo token (usar refresh token dps?) {func}
+// Se não tiver expirado, defina o token do tokenClient
+// Se não existir: faça request de um novo token e salve-o, junto do seu time de expiração
+// Ao fazer logout, apague o token
+// Executar {func} ao fazer qualquer operação com a API
 
-// Criar componente pro InputBox se for necessário outro botão com state disabled
-// Tornar cidade um DropdownSelect (opcional)
-// Requisitos:
-// - Caixa de pesquisa nativa opcional para filtragem (tanto em cidade como estado)
-// - Pesquisa lowercase sem acentos
-// - Desabilitar caixa de cidade até que o estado seja selecionado
-// - Ao trocar estado, redefinir a caixa cidade
+// function handleSignoutClick() {
+//     const token = gapi.client.getToken();
+//     if (token !== null) {
+//       google.accounts.oauth2.revoke(token.access_token);
+//       gapi.client.setToken("");
+//       // document.getElementById("content").innerText = "";
+//       // document.getElementById("authorize_button").innerText = "Authorize";
+//       // document.getElementById("signout_button").style.visibility = "hidden";
+//     }
+//   }
---
+++ src/Home.jsx
@@ -0,0 +1,145 @@
+import React from "react";
+import MedicalRecord from "./MedicalRecord";
+import RegistrationEditor from "./RegistrationEditor";
+import AnamnesisEditor from "./AnamnesisEditor";
+import SessionLogEditor from "./SessionLogEditor";
+import storage from "./storage";
+import utils from "./utils";
+import "./Home.css";
+import "./ModalBodyElements.css";
+
+export default class Home extends React.Component {
+  constructor() {
+    super();
+    // The "pid" (Patient Identifier) is the index of the selected patient in
+    // the stored array of patients.
+    this.state = {
+      patientsNames: storage.getAllPatientsNames(),
+      nameFilter: "",
+      displayedModal: null,
+      pid: null,
+    };
+  }
+
+  getFilteredNames() {
+    return this.state.patientsNames.filter((name) => {
+      name = utils.removeAccents(name).toLowerCase();
+      const nameFilter = utils.removeAccents(this.state.nameFilter).toLowerCase().trim();
+      return name.includes(nameFilter);
+    });
+  }
+
+  getDisplayedModal(args) {
+    switch (this.state.displayedModal) {
+      case "MedicalRecord":
+        return <MedicalRecord {...args} />;
+
+      case "RegistrationEditor":
+        return <RegistrationEditor {...args} />;
+
+      case "AnamnesisEditor":
+        return <AnamnesisEditor {...args} />;
+
+      case "SessionLogEditor":
+        return <SessionLogEditor {...args} />;
+    }
+  }
+
+  closeModal = () => {
+    this.setState({
+      displayedModal: null,
+      pid: null,
+      patientsNames: storage.getAllPatientsNames(),
+    });
+  };
+
+  render() {
+    return (
+      <>
+        {/* Header */}
+        <header id="header">
+          <img src="profile-picture.png" alt="profile picture" />
+        </header>
+
+        {/* Inputs container */}
+        <div id="inputs-container">
+          <div className="input-box grow">
+            <img src="search.png" alt="search patient" />
+            <input
+              type="text"
+              placeholder="Pesquisar..."
+              value={this.state.nameFilter}
+              onInput={(e) => {
+                const raw = e.target.value;
+                const value = raw.trim() + (raw.endsWith(" ") && raw.length > 1 ? " " : "");
+                this.setState({ nameFilter: value });
+              }}
+            />
+          </div>
+
+          <div
+            className="add-patient input-box pointer"
+            onClick={() => this.setState({ displayedModal: "RegistrationEditor" })}
+          >
+            <img src="add-patient.png" alt="add patient" />
+            <p>Novo Paciente</p>
+          </div>
+        </div>
+
+        {/* Main */}
+        <main id="main-area">
+          {this.getFilteredNames().map((name, i) => (
+            <div key={i} className="row" onClick={() => this.setState({ pid: i })}>
+              <img src="patient-picture.png" alt="patient" />
+              <p
+                className="name pointer"
+                onClick={() => this.setState({ displayedModal: "MedicalRecord" })}
+              >
+                {name}
+              </p>
+              <div className="grow"></div>
+              <div
+                className="input-box pointer"
+                onClick={() => this.setState({ displayedModal: "RegistrationEditor" })}
+              >
+                Cadastro
+              </div>
+              <div
+                className="input-box pointer"
+                onClick={() => this.setState({ displayedModal: "AnamnesisEditor" })}
+              >
+                Anamnese
+              </div>
+              <div
+                className="input-box pointer"
+                onClick={() => this.setState({ displayedModal: "SessionLogEditor" })}
+              >
+                Evolução
+              </div>
+            </div>
+          ))}
+        </main>
+
+        {/* Modal */}
+        {this.getDisplayedModal({
+          pid: this.state.pid,
+          onClose: this.closeModal,
+        })}
+      </>
+    );
+  }
+}
+
+// Deletar registros de sessões e pacientes
+// Integrar com o Drive (fazer caching)
+// Adicionar opção de anexar documentos no paciente
+// Adicionar opção imprimir prontuário
+// Tornar pago (R$ 20 / mes)
+
+// Criar componente pro InputBox se for necessário outro botão com state disabled
+// Tornar cidade um DropdownSelect (opcional)
+// Requisitos:
+// - Caixa de pesquisa nativa opcional para filtragem (tanto em cidade como estado)
+// - Pesquisa lowercase sem acentos
+// - Desabilitar caixa de cidade até que o estado seja selecionado
+// - Ao trocar estado, redefinir a caixa cidade
