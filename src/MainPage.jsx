f4af021652e5b6396cd1e5e3b33aa3ca22a77656
Kelio <keliojosue2@gmail.com>
2023-03-02 13:24:33 -0300

    Saving changes

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -14,19 +14,32 @@ export default class MainPage extends React.Component {
     super();
     // The "pid" (Patient Identifier) is the index of the selected patient in
     // the stored array of patients.
+    // The "displayedDropdownId" value is set to the id of the target patient when the user
+    // clicks in the edit button. It is used to identify the dropdown that must be displayed.
     this.state = {
-      patientsNames: storage.getAllPatientsNames(),
       nameFilter: "",
+      displayedDropdownId: null,
       displayedModal: null,
       pid: null,
     };
   }
 
-  getFilteredNames() {
-    return this.state.patientsNames.filter((name) => {
-      name = utils.removeAccents(name).toLowerCase();
-      const nameFilter = utils.removeAccents(this.state.nameFilter).toLowerCase().trim();
-      return name.includes(nameFilter);
+  componentDidMount() {
+    window.onclick = () => this.setState({ displayedDropdownId: null });
+  }
+
+  componentWillUnmount() {
+    window.onclick = null;
+  }
+
+  getPatients(nameFilter) {
+    nameFilter = nameFilter || "";
+    const patients = storage.getAllPatients().map((p, i) => ({ name: p.cadastro.nome, id: i }));
+
+    return patients.filter((p) => {
+      p.name = utils.removeAccents(p.name).toLowerCase();
+      nameFilter = utils.removeAccents(nameFilter).toLowerCase().trim();
+      return p.name.includes(nameFilter);
     });
   }
 
@@ -50,7 +63,6 @@ export default class MainPage extends React.Component {
     this.setState({
       displayedModal: null,
       pid: null,
-      patientsNames: storage.getAllPatientsNames(),
     });
   };
 
@@ -70,7 +82,7 @@ export default class MainPage extends React.Component {
               type="text"
               placeholder="Pesquisar..."
               value={this.state.nameFilter}
-              onInput={(e) => {
+              onChange={(e) => {
                 const raw = e.target.value;
                 const value = raw.trim() + (raw.endsWith(" ") && raw.length > 1 ? " " : "");
                 this.setState({ nameFilter: value });
@@ -86,27 +98,51 @@ export default class MainPage extends React.Component {
 
         {/* Main */}
         <main id="main-area">
-          {this.getFilteredNames().map((name, i) => (
-            <div key={i} className="row" onClick={() => this.setState({ pid: i })}>
-              <img src="patient-picture.png" />
+          {this.getPatients(this.state.nameFilter).map((p) => (
+            <div key={p.id} className="row">
+              <img className="patient-icon" src="patient-picture.png" />
               <p
                 className="name pointer"
-                onClick={() => this.setState({ displayedModal: "MedicalRecord" })}
+                onClick={() => this.setState({ displayedModal: "MedicalRecord", pid: p.id })}
               >
-                {name}
+                {p.name}
               </p>
               <div className="grow"></div>
-              <InputBox onClick={() => this.setState({ displayedModal: "RegistrationEditor" })}>
-                Cadastro
-              </InputBox>
-
-              <InputBox onClick={() => this.setState({ displayedModal: "AnamnesisEditor" })}>
-                Anamnese
-              </InputBox>
-
-              <InputBox onClick={() => this.setState({ displayedModal: "SessionLogEditor" })}>
-                Evolução
-              </InputBox>
+              <div className="dropdown-edit">
+                <img
+                  className="edit-icon pointer"
+                  src="edit.png"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    this.setState({ displayedDropdownId: p.id });
+                  }}
+                />
+                {this.state.displayedDropdownId === p.id && (
+                  <div className="options pointer">
+                    <p
+                      onClick={() =>
+                        this.setState({ displayedModal: "RegistrationEditor", pid: p.id })
+                      }
+                    >
+                      Cadastro
+                    </p>
+                    <p
+                      onClick={() =>
+                        this.setState({ displayedModal: "AnamnesisEditor", pid: p.id })
+                      }
+                    >
+                      Anamnese
+                    </p>
+                    <p
+                      onClick={() =>
+                        this.setState({ displayedModal: "SessionLogEditor", pid: p.id })
+                      }
+                    >
+                      Evolução
+                    </p>
+                  </div>
+                )}
+              </div>
             </div>
           ))}
         </main>
